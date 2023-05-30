a6cd3d411818e1ed22c9426465fc345658393e27
Darius Horvath <dariushorvath33@gmail.com>
2023-05-30 15:43:43 +0200

    Implemented cache update logic, (instant refresh on like or on tweet post)

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -34,6 +34,7 @@ function Form() {
   }, []);
 
   if (session.status !== "authenticated") return null;
+  const trpcUtils = api.useContext();
 
   useLayoutEffect(() => {
     updateTextAreaSize(textAreaRef.current);
@@ -44,6 +45,35 @@ function Form() {
     onSuccess: (newTweet) => {
       // Parameter onSuccess runs if the operation was a success
       setInputValue("");
+
+      if (session.status !== "authenticated") return;
+      
+
+      trpcUtils.tweet.infiniteFeed.setInfiniteData({}, (oldData) => {
+        if (oldData == null || oldData.pages[0] == null) return;
+
+        const newCacheTweet = {
+          ...newTweet,
+          likeCount: 0,
+          likedByMe: false,
+          user: {
+            id: session.data.user.id,
+            name: session.data.user.name || null,
+            image: session.data.user.image || null
+          },
+        };
+
+        return {
+          ...oldData,
+          pages: [
+            {
+              ...oldData.pages[0],
+              tweets: [newCacheTweet, ...oldData.pages[0].tweets],
+            },
+            ...oldData.pages.slice(1),
+          ]
+        }
+      });
     },
   });
 
